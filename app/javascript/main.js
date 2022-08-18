81c641974aa82f10fbe6dd153cfdc10a3396196a
shinopai <harbor.shinozaki@gmail.com>
2022-08-18 09:40:41 +0000

    complete

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,12 +1,4 @@
 $(function(){
-  // slick
-  $('.slider').slick({
-        autoplay: true,
-        autoplaySpeed: 4000,
-        speed: 700,
-        dots: true
-  })
-
   // drawer
   var headerIcon = $('.header__icon');
   var drawerOverlay = $('.drawer__overlay');
---
+++ app/javascript/slick.js
@@ -0,0 +1,9 @@
+$(function(){
+  // slick
+  $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 700,
+        dots: true
+  })
+})
